d4d0d4fe20d052fdf86aecc3d62774f18938d211
bwiley1 <bewi7122@colorado.edu>
2015-11-04 18:08:14 -0700

    Generalized corrections to code and txt file

--- categories/findCategories.py
+++ categories/findCategories.py
@@ -2,8 +2,8 @@ import cPickle as pickle
 from collections import defaultdict
 import json
 
-pages = pickle.load(open('C:/Users/Benjamin/Desktop/Project/wiki_pages.pkl', 'rb'))
-correct = pickle.load(open('C:/Users/Benjamin/Desktop/Project/correct.pkl', 'rb'))
+pages = pickle.load(open('../wiki_pages.pkl', 'rb'))
+correct = pickle.load(open('../correct.pkl', 'rb'))
 
 # Initialize list of categories
 categories = defaultdict(dict)
@@ -13,4 +13,4 @@ for i in xrange(len(pages)):
         if pages[i]['title'].lower() in str(correct[j]).lower():
             categories[j] = pages[i]['categories']
 
-pickle.dump(categories, open("C:/Users/Benjamin/Desktop/Project/categories/categories.pkl", 'wb'))
+pickle.dump(categories, open("../categories.pkl", 'wb'))
