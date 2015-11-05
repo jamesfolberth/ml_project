dc2d0f1d703aad5e1c7a5ed60e8b6166f4f01423
bwiley1 <bewi7122@colorado.edu>
2015-11-04 23:25:17 -0700

    Updates
    Still need to work on bugs in the answer categories generation...

--- categories/findCategories.py
+++ categories/findCategories.py
@@ -1,15 +1,31 @@
 import cPickle as pickle
 from collections import defaultdict
 
+'''
+Load wiki pages, correct answers, and unique answers
+'''
 pages = pickle.load(open('../wiki_pages.pkl', 'rb'))
 correct = pickle.load(open('../correct.pkl', 'rb'))
+ans = pickle.load(open('../ans.pkl', 'rb'))
 
 # Initialize list of categories
-categories = defaultdict(dict)
+correct_cat = defaultdict(dict)
+ans_cat = defaultdict(dict)
 
-for i in xrange(len(pages)):
-    for j in xrange(len(correct)):
-        if pages[i]['title'].lower() in str(correct[j]).lower():
-            categories[j] = pages[i]['categories']
+# Generate categories for answers to questions (i.e. genres for questions)
+for i in xrange(len(correct)):
+    for j in xrange(len(pages)):
+        if pages[j]['title'].lower() in str(correct[i]).lower():
+            correct_cat[i] = pages[j]['categories']
 
-pickle.dump(categories, open("../categories.pkl", 'wb'))
+# Generate database for answer type (i.e. genres for answers)
+for i in xrange(len(ans)):
+    for j in xrange(len(pages)):
+        if ans[i].lower() in pages[j]['title'].lower():
+            ans_cat[ans[i]] = pages[j]['categories']
+
+'''
+Save data
+'''
+# pickle.dump(categories, open("../correct_cat.pkl", 'wb'))
+# pickle.dump(categories, open("../ans_cat.pkl", 'wb'))
