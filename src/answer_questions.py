23677d3ce070a574378f224aa167527f589efb45
James Folberth <jamesfolberth@gmail.com>
2015-11-05 12:53:33 -0700

    Simple check of pages_dict in answer_questions.py

--- src/answer_questions.py
+++ src/answer_questions.py
@@ -4,6 +4,29 @@ import argparse
 
 import cPickle as pickle
 
+import read_csv_data
+
+def run_development(args):
+    """
+    Do development things, such as computing and saving features, X-val, etc.
+    """
+    
+    train_reader = read_csv_data.read_csv_data('../data/sci_train.csv')
+    pages_dict = pickle.load(open('../data/wiki_pages_dict.pkl', 'rb'))
+    
+    # iterate over all answers and print the corresponding wiki page title
+    # as a check that pages_dict has all the right keys
+    for row in train_reader:
+        for ans in (row['answerA'], row['answerB'], row['answerC'], row['answerD']):
+            print (u"answer: {}\n  title: {}".format(ans, pages_dict[ans]['title']))
+
+
+def make_predictions(args):
+    """
+    Make predictions on the full test set.
+    """
+    raise NotImplementedError("TODO")
+
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
@@ -19,7 +42,7 @@ if __name__ == "__main__":
             type=bool, default=False, required=False)
 
     args = argparser.parse_args()
-
+    
+    run_development(args)
     #make_predictions(args)
-    #run_development(args)
 
